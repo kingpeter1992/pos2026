d061a9ba24d88ec56620ad98cff63c53c787bec8
Jean-Pierre KAPETA <jean-pierre.kapeta@tractafric.com>
2026-04-10 22:23:21 +0200

    Initial commit propre + dashboard connecté store

--- dist/peace-app/browser/chunk-MNPB53NW.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-PNGC45FH.js";import"./chunk-VB56BUGO.js";export default a();
--- src/environnement/environment.ts
+++ src/environnement/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-
+/*
  production: false,
   BASIC_URL: "http://127.0.0.1:8083/api/",
   BASIC_URL_AUTH_LOGIN: "http://127.0.0.1:8083/api/auth/login",
@@ -7,13 +7,13 @@ export const environment = {
   BASIC_URL_FORGOT: "http://127.0.0.1:8083/api/auth/forgot-password",
   BASIC_URL_RENITIALISATION: "http://127.0.0.1:8083/api/auth/reset-password",
   BASIC_URL_ADMIN: "http://127.0.0.1:8083/api/admin"
-   /*
+   */
     production: true,
-  BASIC_URL: "https://back-peace2026-1.onrender.com/api/",
-  BASIC_URL_AUTH_LOGIN: "https://back-peace2026-1.onrender.com/api/auth/login",
-  BASIC_URL_REGISTER: "https://back-peace2026-1.onrender.com/api/auth/register",
-  BASIC_URL_FORGOT: "https://back-peace2026-1.onrender.com/api/auth/forgot-password",
-  BASIC_URL_RENITIALISATION: "https://back-peace2026-1.onrender.com/api/auth/reset-password",
-  BASIC_URL_ADMIN: "https://back-peace2026-1.onrender.com/api/admin"
-  */
+  BASIC_URL: "https://back-pos2026.onrender.com/api/",
+  BASIC_URL_AUTH_LOGIN: "https://back-pos2026.onrender.com/api/auth/login",
+  BASIC_URL_REGISTER: "https://back-pos2026.onrender.com/api/auth/register",
+  BASIC_URL_FORGOT: "https://back-pos2026.onrender.com/api/auth/forgot-password",
+  BASIC_URL_RENITIALISATION: "https://back-pos2026.onrender.com/api/auth/reset-password",
+  BASIC_URL_ADMIN: "https://back-pos2026.onrender.com/api/admin"
+
 };
