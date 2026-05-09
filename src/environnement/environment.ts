98fab6aa12d07284e7f89336361f29faad423d57
Jean-Pierre KAPETA <jean-pierre.kapeta@tractafric.com>
2026-05-09 20:12:23 +0200

    Dashboard POS ultra premium + requisition + roles operator + rapports ventes URL

--- src/environnement/environment.ts
+++ src/environnement/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
-
- production: false,
+/*  production: false,
   BASIC_URL: "http://127.0.0.1:8083/api/",
   BASIC_URL_AUTH_LOGIN: "http://127.0.0.1:8083/api/auth/login",
   BASIC_URL_REGISTER: "http://127.0.0.1:8083/api/auth/register",
   BASIC_URL_FORGOT: "http://127.0.0.1:8083/api/auth/forgot-password",
   BASIC_URL_RENITIALISATION: "http://127.0.0.1:8083/api/auth/reset-password",
   BASIC_URL_ADMIN: "http://127.0.0.1:8083/api/admin"
-   /*
+   */
     production: true,
   BASIC_URL: "https://back-pos2026.onrender.com/api/",
   BASIC_URL_AUTH_LOGIN: "https://back-pos2026.onrender.com/api/auth/login",
@@ -15,5 +14,5 @@ export const environment = {
   BASIC_URL_FORGOT: "https://back-pos2026.onrender.com/api/auth/forgot-password",
   BASIC_URL_RENITIALISATION: "https://back-pos2026.onrender.com/api/auth/reset-password",
   BASIC_URL_ADMIN: "https://back-pos2026.onrender.com/api/admin"
-*/
+
 };
